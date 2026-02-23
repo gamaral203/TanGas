4b1367abf4096b593a06c59b1e727b1ea34a4ac9
Gabriel amaral <amaralgabriel357@gmail.com>
2026-02-23 18:09:09 -0300

    feat: cria a classe ProdutoDTO para a segurança da camada de model

--- tangas/src/main/java/br/com/tangas/tangas/dto/ProdutoDTO.java
+++ tangas/src/main/java/br/com/tangas/tangas/dto/ProdutoDTO.java
@@ -1,4 +1,21 @@
 package br.com.tangas.tangas.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutoDTO {
+
+
+    private Long id;
+    private String nome;
+    private Integer quantidade;
+    private BigDecimal preco;
+
 }
