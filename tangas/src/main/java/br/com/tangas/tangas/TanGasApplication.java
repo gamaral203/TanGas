8beaabbda8b0b4e72c4fb12ad65293c833872889
Gabriel amaral <amaralgabriel357@gmail.com>
2026-02-23 16:17:52 -0300

    chore:inicio do projeto, criando o arquivo principal

---
+++ tangas/src/main/java/br/com/tangas/tangas/TanGasApplication.java
@@ -0,0 +1,13 @@
+package br.com.tangas.tangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanGasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanGasApplication.class, args);
+	}
+
+}
---
+++ tangas/src/test/java/br/com/tangas/tangas/TanGasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tangas.tangas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanGasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
