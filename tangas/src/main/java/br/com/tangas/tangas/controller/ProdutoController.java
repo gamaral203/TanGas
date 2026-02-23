6c2c7f12b1386f6c6013583e4479b0cdfba16bd0
Gabriel amaral <amaralgabriel357@gmail.com>
2026-02-23 18:17:20 -0300

    feat: cria a classe ProdutoController

---
+++ tangas/src/main/java/br/com/tangas/tangas/controller/ProdutoController.java
@@ -0,0 +1,19 @@
+package br.com.tangas.tangas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    //Cria produtos
+
+    //EditaProdutos
+
+    //ListaProdutos
+
+    //pesquisarProdutosID
+
+    //DeletarProduto
+}
